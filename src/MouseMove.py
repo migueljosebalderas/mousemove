513edecdf3e94d0f69cae978700292d2025adf1e
Miguel Balderas <migueljbd@gmail.com>
2020-03-31 23:03:33 -0600

    inicial

---
+++ src/MouseMove.py
@@ -0,0 +1,13 @@
+import pyautogui as mouse
+import time
+
+while True:
+    mouse.moveTo(100, 100, duration=2)
+    time.sleep(5)
+    mouse.moveTo(200, 100, duration=2)
+    time.sleep(5)
+    mouse.moveTo(200, 200, duration=2)
+    time.sleep(5)
+    mouse.moveTo(100, 200, duration=2)
+    time.sleep(5)
+    mouse.moveTo(100, 200, duration=2)
